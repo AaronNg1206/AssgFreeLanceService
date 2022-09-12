d12388e7465d0b12f547db14a883b229c9d5f6d0
Aaron <zonghao6021@gmail.com>
2022-09-12 15:46:50 +0800

    Option menu User Profile

--- app/src/main/java/com/nghycp/assgfreelanceservice/UserProfileActivity.kt
+++ app/src/main/java/com/nghycp/assgfreelanceservice/UserProfileActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.nghycp.assgfreelanceservice.databinding.ActivityUserProfileBinding
 
 class UserProfileActivity : AppCompatActivity() {
@@ -15,4 +16,24 @@ class UserProfileActivity : AppCompatActivity() {
         binding = ActivityUserProfileBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_user_profile)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.userprofile, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.btn_edit_user -> {
+                startActivity(Intent(this,LoginActivity::class.java))
+            }
+            R.id.back_user_btn -> {
+                startActivity(Intent(this,UserHomePage::class.java))
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
