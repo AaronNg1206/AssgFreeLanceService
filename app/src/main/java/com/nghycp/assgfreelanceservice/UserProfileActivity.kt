6e2c439bc3267640b285f8dcf2f27ff78d5c3115
Aaron <zonghao6021@gmail.com>
2022-09-14 00:30:42 +0800

    error User profile

--- app/src/main/java/com/nghycp/assgfreelanceservice/UserProfileActivity.kt
+++ app/src/main/java/com/nghycp/assgfreelanceservice/UserProfileActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+//import
 import android.view.MenuItem
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
