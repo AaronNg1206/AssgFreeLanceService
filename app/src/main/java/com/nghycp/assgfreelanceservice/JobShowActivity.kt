21a30d9594bdd19bef3bdb1da4221758f96676ab
cmyeap01 <cmyeap123@gmail.com>
2022-09-26 00:18:15 +0800

    cm JobShow

---
+++ app/src/main/java/com/nghycp/assgfreelanceservice/JobShowActivity.kt
@@ -0,0 +1,11 @@
+package com.nghycp.assgfreelanceservice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JobShowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_job_show)
+    }
+}
